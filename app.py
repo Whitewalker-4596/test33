13f14fd6d28fa9f09b7594a2159601d2d680d490
Whitewalker-459 <130918267+Whitewalker-4596@users.noreply.github.com>
2023-04-16 19:09:13 +0000

    bootstrap added

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
   return render_template("home.html")
 
 
+# bootstrap link =  " https://getbootstrap.com/docs/5.3/getting-started/introduction/ "
+
+# html beginner = " https://htmldog.com/guides/html/beginner/ "
+
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',debug=True)
+  app.run(host='0.0.0.0', debug=True)
